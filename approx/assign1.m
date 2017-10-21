a0f113889e75a1ab58bc27693f5f6d7edfbb760c
criss <criss.lin@hotmail.com>
2017-10-21 16:08:03 +0800

    linewidth and comment of chebyshev

--- approx/assign1.m
+++ approx/assign1.m
@@ -9,21 +9,21 @@ addpath('./util')
 %% ground truth
 x = linspace(-5,5);
 y = f(x);
-plot(x,y)
+plot(x,y,'linewidth',2)
 hold on
 
 %% equally spaced points
 xs1 = -5:1:5;
 ys1 = f(xs1);
 y1 = lagrange(x, xs1, ys1);
-plot(x, y1, '--')
+plot(x, y1, '--','linewidth',2)
 hold on
 
 %% zero points of order 11 Chebyshev polynomial
 xs2 = 5*zero_point_of_chebyshev(11, 1:11);
 ys2 = f(xs2);
 y2 = lagrange(x, xs2, ys2);
-plot(x, y2, '-.m')
+plot(x, y2, '-.m','linewidth',2)
 hold off
 legend('ground truth', 'equally spaced', 'chebyshev')
 title('Lagrange Interpolation of $f(x)=\frac{1}{1+x^2}, x \in [-5, 5]$','Interpreter','LaTex')
--- approx/util/lagrange_base.m
+++ approx/util/lagrange_base.m
@@ -1,5 +1,5 @@
 function [y] = lagrange_base(x, xs, i)
-%% l_i(x) = \sum{(x-x_1)...(x-x_{i-1})(x-x{i+1}...(x-x_{n+1}))} / \sum{(x_i-x_1)...(x_i-x_{i-1})(x_i-x{i+1}...(x_i-x_{n+1}))}
+%% l_i(x) = \sum{(x-x_1)...(x-x_{i-1})(x-x_{i+1})...(x-x_{n+1})/ (x_i-x_1)...(x_i-x_{i-1})(x_i-x_{i+1})...(x_i-x_{n+1})}
 % @param x - input x
 % @param xs - x_1, x_2, ...
 % @param i - 1-based ith lagrange base
